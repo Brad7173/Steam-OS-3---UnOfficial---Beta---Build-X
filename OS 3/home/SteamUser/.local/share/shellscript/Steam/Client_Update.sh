caa5c3d663ffa7e7c72c80ff78872e7bfa760364
Brad7173 <94210825+Brad7173@users.noreply.github.com>
2024-03-31 17:55:17 -0500

    Add files via upload

---
+++ OS 3/home/SteamUser/.local/share/shellscript/Steam/Client_Update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Script by Katie M. Nelson 3/27/24
+UserPath=$(echo "/home/""$USER")
+echo "osd-level=0" >> ~/.config/mpv/mpv.conf
+echo "fs=yes" >> ~/.config/mpv/mpv.conf
+echo "osc=no" >> ~/.config/mpv/mpv.conf
+mpv --fs $UserPath/.local/share/Steam_Movies/System_Default/Default_Checking_Client_Updates_White_FFFFFF.webm
+rm ~/.config/mpv/mpv.conf
+exit
